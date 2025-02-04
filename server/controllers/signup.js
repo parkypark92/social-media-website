f8b9f07a41dc8115d5ea298ea0af78b9abba497e
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-02-04 11:29:09 +0000

    implement error handling for unique constraints and password confirmation in signup API

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -2,6 +2,29 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports.process_signup = async (req, res, next) => {
+  const errors = [];
+  const name = await prisma.user.findUnique({
+    where: {
+      username: req.body.formData.username,
+    },
+  });
+  const email = await prisma.user.findUnique({
+    where: {
+      email: req.body.formData.email,
+    },
+  });
+  if (name) {
+    errors.push("Username already in use, please choose another username");
+  }
+  if (email) {
+    errors.push("Email already in use, please choose another email");
+  }
+  if (req.body.formData.passwordConfirm !== req.body.formData.password) {
+    errors.push("Password not correctly confirmed");
+  }
+  if (errors.length) {
+    return res.json({ msgs: errors, status: 400 });
+  }
   const user = await prisma.user.create({
     data: {
       username: req.body.formData.username,
@@ -10,6 +33,6 @@ module.exports.process_signup = async (req, res, next) => {
     },
   });
   if (user) {
-    res.json({ success: true, message: "User created", statusCode: 200, user });
+    res.json({ success: true, message: "User created", status: 200 });
   }
 };
