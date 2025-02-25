a8e62e9007c3d8130620ce9e6ca8b56703631a67
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-02-25 20:05:15 +0000

    define a Friendship schema model which tracks requests between users

---
+++ server/prisma/migrations/20250225200241_implement_friendship_model/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_UserFollows` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_UserFollows" DROP CONSTRAINT "_UserFollows_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_UserFollows" DROP CONSTRAINT "_UserFollows_B_fkey";
+
+-- DropTable
+DROP TABLE "_UserFollows";
+
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "id" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friendship_senderId_receiverId_key" ON "Friendship"("senderId", "receiverId");
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
