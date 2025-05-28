68a4d721708f94865b2ab07a59d351effa725f60
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-05-28 12:29:24 +0100

    refactor Messages to use react query to fetch conversations

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -2,6 +2,7 @@
 // import socket from "../config/socket";
 import { useOutletContext } from "react-router-dom";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 import { useState, useEffect } from "react";
 import Chats from "../components/messenger/Chats";
 import MessageBox from "../components/messenger/MessageBox";
@@ -13,21 +14,53 @@ export default function Messages() {
   const [allChats, setAllChats] = useState([]);
 
   const { user } = useOutletContext();
+
+  const fetchConversations = async () => {
+    const response = await axios.get(
+      "http://localhost:3000/users/conversations",
+      { params: { id: user.id } }
+    );
+    if (response.status === 200) {
+      return response.data.chats ? response.data : [];
+    } else {
+      throw new Error("Error retrieving messeges");
+    }
+  };
+
+  const conversationsQuery = useQuery({
+    queryKey: ["conversations", user.id],
+    queryFn: fetchConversations,
+  });
+
   useEffect(() => {
-    const fetchConversations = async () => {
-      const response = await axios.get(
-        "http://localhost:3000/users/conversations",
-        { params: { id: user.id } }
-      );
-      if (response.data.chats.length) {
-        setAllChats(response.data.chats);
-        setCurrentChat(response.data.chats[0]);
+    if (conversationsQuery.isSuccess) {
+      if (conversationsQuery.data.chats.length > 0) {
+        setAllChats(conversationsQuery.data.chats);
+        setCurrentChat(conversationsQuery.data.chats[0]);
       } else {
         setNewChat(true);
       }
-    };
-    fetchConversations();
-  }, [user.id]);
+    }
+  }, [conversationsQuery.data?.chats, conversationsQuery.isSuccess]);
+
+  // useEffect(() => {
+  //   const fetchConversations = async () => {
+  //     const response = await axios.get(
+  //       "http://localhost:3000/users/conversations",
+  //       { params: { id: user.id } }
+  //     );
+  //     if (response.data.chats.length) {
+  //       setAllChats(response.data.chats);
+  //       setCurrentChat(response.data.chats[0]);
+  //     } else {
+  //       setNewChat(true);
+  //     }
+  //   };
+  //   fetchConversations();
+  // }, [user.id]);
+  if (conversationsQuery.isLoading) return <h2>Loading...</h2>;
+  if (conversationsQuery.isError)
+    return <h2>{conversationsQuery.error.message}</h2>;
 
   return (
     <div className={styles.pageCtnr}>
