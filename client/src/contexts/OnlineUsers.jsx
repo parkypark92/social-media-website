d17645d7e6da6e1827fd9c75f6704548f557f9c7
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-08-11 17:17:37 +0100

    set up OnlineUsers context

---
+++ client/src/contexts/OnlineUsers.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSocket } from "./SocketProvider";
+import PropTypes from "prop-types";
+
+const OnlineUsersContext = createContext();
+
+export function OnlineUsersProvider({ children }) {
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const socket = useSocket();
+
+  useEffect(() => {
+    socket.on("online-users", (users) => {
+      setOnlineUsers(users);
+    });
+  }, [socket]);
+
+  return (
+    <OnlineUsersContext.Provider value={{ onlineUsers }}>
+      {children}
+    </OnlineUsersContext.Provider>
+  );
+}
+
+export function useOnlineUsers() {
+  return useContext(OnlineUsersContext);
+}
+
+OnlineUsersProvider.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.node,
+};
