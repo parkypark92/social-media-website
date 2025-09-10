fc8c706d2bc07af77e47c25a9ebb3bddd1033f9d
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-09-10 18:01:51 +0100

    implement controller for creating friend request notification

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -425,7 +425,17 @@ module.exports.create_comment_notification = asyncHandler(
 );
 
 module.exports.create_friend_request_notification = asyncHandler(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const notification = await prisma.notification.create({
+      data: {
+        type: req.body.type,
+        message: req.body.message,
+        recipientId: req.body.recipientId,
+        senderId: req.body.senderId,
+      },
+    });
+    res.status(200).json({ notification });
+  }
 );
 
 module.exports.create_accepted_request_notification = asyncHandler(
