b702e958d070e376f00feb6de0328a002f9778ac
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-09-30 10:59:37 +0100

    when receiving a message, if the url path is on /messages, update lastMessageSeenNotification to true

--- client/src/contexts/MessagesProvider.jsx
+++ client/src/contexts/MessagesProvider.jsx
@@ -1,11 +1,18 @@
-import { createContext, useContext } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { createContext, useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSocket } from "./SocketProvider.jsx";
 import axios from "axios";
 import PropTypes from "prop-types";
 
 const MessagesContext = createContext();
 
 export function MessagesProvider({ userId, children }) {
+  const socket = useSocket();
+  const queryClient = useQueryClient();
+  const { pathname } = useLocation();
+  const lastPartOfUrlPath = pathname.split("/").pop();
+
   const fetchConversations = async () => {
     const response = await axios.get(
       "http://localhost:3000/users/conversations",
@@ -23,6 +30,28 @@ export function MessagesProvider({ userId, children }) {
     queryFn: fetchConversations,
     enabled: userId !== undefined,
   });
+
+  const handleMessageNotificationSeen = async (userId) => {
+    const response = await axios.post(
+      "http://localhost:3000/users/message-notifications-seen",
+      { params: userId }
+    );
+    if (response.status === 200) {
+      return response.data.updatedConversations;
+    }
+  };
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("receive-message", (message) => {
+      console.log(`Message: ${message.content}, From: ${message.senderId}`);
+      if (lastPartOfUrlPath === "messages") {
+        handleMessageNotificationSeen(userId);
+      }
+      queryClient.invalidateQueries({ queryKey: ["conversations", userId] });
+    });
+    return () => socket.off("receive-message");
+  }, [lastPartOfUrlPath, queryClient, socket, userId]);
 
   return (
     <MessagesContext.Provider value={conversationsQuery}>
