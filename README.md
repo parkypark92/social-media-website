2513431c1531d4cf78bf92338f6b2632cc0d5c2b
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-10-07 16:20:26 +0100

    change limit to 10

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # social-media-website
+
+Heart icons created by Freepik - Flaticon
+Profile icon created by Becris - Flaticon
+Home icon created by Freepik - Flaticon
+Message icon created by Freepik - Flaticon
+Logout icon created by Pixel perfect - Flaticon
+Send icons created by Tanah Basah - Flaticon
+
+//TODO
+
+- Ability to save posts?
+- Implement pagination for loading posts on the feed
+- style login error messages
+- style signup form
+- style signup error messages
+- only show 10 latest notifications
+- if a chat has no messages, move it back to new chats
--- client/src/components/posts/PostsFeed.jsx
+++ client/src/components/posts/PostsFeed.jsx
@@ -10,7 +10,7 @@ export default function PostsFeed() {
   const friendIds = friendsList.map((friend) => friend.id);
 
   const fetchPostData = async ({ pageParam = 1 }) => {
-    const limit = 5;
+    const limit = 10;
     const response = await axios.get("http://localhost:3000/users/get-posts", {
       params: { ids: friendIds, page: pageParam, limit },
     });
@@ -74,55 +74,3 @@ export default function PostsFeed() {
     </div>
   );
 }
-
-// import Post from "./Post";
-// import { useState, useEffect } from "react";
-// import { useOutletContext } from "react-router-dom";
-// import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
-
-// export default function PostsFeed() {
-//   const [feedError, setFeedError] = useState(null);
-//   const { friendsList } = useOutletContext();
-//   const friendIds = friendsList.map((friend) => friend.id);
-
-//   const fetchPostData = async () => {
-//     const response = await axios.get("http://localhost:3000/users/get-posts", {
-//       params: { ids: friendIds },
-//     });
-//     if (response.data.posts) {
-//       return response.data;
-//     } else {
-//       throw new Error("Error loading posts feed");
-//     }
-//   };
-
-//   const postsQuery = useQuery({
-//     queryKey: ["posts", friendIds],
-//     queryFn: fetchPostData,
-//   });
-
-//   useEffect(() => {
-//     if (postsQuery.isSuccess) {
-//       setFeedError(null);
-//     }
-//     if (postsQuery.isError) {
-//       setFeedError(postsQuery.error.message);
-//     }
-//   }, [postsQuery.error?.message, postsQuery.isError, postsQuery.isSuccess]);
-
-//   if (postsQuery.isLoading) return <h2>Loading feed...</h2>;
-
-//   return (
-//     <div>
-//       {feedError && <h2>{feedError}</h2>}
-//       {postsQuery.data.posts.length > 0 ? (
-//         postsQuery.data.posts.map((post) => {
-//           return <Post key={post.id} postContent={post} feedPost={true} />;
-//         })
-//       ) : (
-//         <h2>No posts to display...</h2>
-//       )}
-//     </div>
-//   );
-// }
