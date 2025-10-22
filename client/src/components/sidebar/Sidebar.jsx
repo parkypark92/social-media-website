2580d63ecb810913478fc8afe9b81eea6953527c
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-10-22 19:23:45 +0100

    make list items into Links to respective pages (still need to implement saved posts)

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,26 @@
 import styles from "./Sidebar.module.css";
+import { Link } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
 export default function Sidebar() {
+  const { user } = useOutletContext();
   return (
     <ul className={styles.list}>
-      <li className={styles.listItem}>Explore</li>
-      <li className={styles.listItem}>Profile</li>
-      <li className={styles.listItem}>Saved</li>
-      <li className={styles.listItem}>Friends</li>
-      <li className={styles.listItem}>Messenger</li>
+      <Link to="/" className={styles.menuLink}>
+        <li className={styles.listItem}>Home</li>
+      </Link>
+      <Link to={`/profile/${user.id}`} className={styles.menuLink}>
+        <li className={styles.listItem}>Profile</li>
+      </Link>
+      <Link to="/" className={styles.menuLink}>
+        <li className={styles.listItem}>Saved</li>
+      </Link>
+      <Link to={`/${user.id}/friends`} className={styles.menuLink}>
+        <li className={styles.listItem}>Friends</li>
+      </Link>
+      <Link to={`/${user.id}/messages`} className={styles.menuLink}>
+        <li className={styles.listItem}>Messenger</li>
+      </Link>
     </ul>
   );
 }
