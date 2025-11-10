431f251fd3ee1c48d4d43d1c1d416989688837d5
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-11-10 11:50:49 +0000

    add icons to sidebar menu

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -7,19 +7,34 @@ export default function Sidebar() {
   return (
     <ul className={styles.list}>
       <Link to="/" className={styles.menuLink}>
-        <li className={styles.listItem}>Home</li>
+        <div className={styles.menuItem}>
+          <img src="/home-black.png" alt="" height={20} />
+          <li className={styles.listItem}>Home</li>
+        </div>
       </Link>
       <Link to={`/profile/${user.id}`} className={styles.menuLink}>
-        <li className={styles.listItem}>Profile</li>
+        <div className={styles.menuItem}>
+          <img src="/profile.png" alt="" height={20} />
+          <li className={styles.listItem}>Profile</li>
+        </div>
       </Link>
       <Link to={`/${user.id}/saved`} className={styles.menuLink}>
-        <li className={styles.listItem}>Saved</li>
+        <div className={styles.menuItem}>
+          <img src="/unsave.png" alt="" height={20} />
+          <li className={styles.listItem}>Saved</li>
+        </div>
       </Link>
       <Link to={`/${user.id}/friends`} className={styles.menuLink}>
-        <li className={styles.listItem}>Friends</li>
+        <div className={styles.menuItem}>
+          <img src="/friends.png" alt="" height={20} />
+          <li className={styles.listItem}>Friends</li>
+        </div>
       </Link>
       <Link to={`/${user.id}/messages`} className={styles.menuLink}>
-        <li className={styles.listItem}>Messenger</li>
+        <div className={styles.menuItem}>
+          <img src="/message-black.png" alt="" height={20} />
+          <li className={styles.listItem}>Messenger</li>
+        </div>
       </Link>
     </ul>
   );
