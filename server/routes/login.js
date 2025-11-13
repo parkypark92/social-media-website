23852741c39582eb7930f35ef4007b9c15ff70c1
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-11-13 11:04:17 +0000

    implement post route for first login API

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,5 +5,6 @@ const { sendErrors } = require("../utils/validation/sendErrors");
 const { validateLogin } = require("../utils/validation/validateLogin");
 
 router.post("/", validateLogin(), sendErrors, controller.process_login);
+router.post("/update-first-login", controller.update_first_login);
 
 module.exports = router;
