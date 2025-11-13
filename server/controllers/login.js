54b0696aea6dc7b3652a53a3f7d73ae303c2b09e
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-11-13 11:03:50 +0000

    implement API for updating first login info

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,4 +1,6 @@
 const { issueJWT } = require("../utils/jwtUtils");
+const asyncHandler = require("express-async-handler");
+const prisma = require("../config/prisma");
 
 module.exports.process_login = (req, res, next) => {
   const tokenObject = issueJWT(req.user);
@@ -10,3 +12,15 @@ module.exports.process_login = (req, res, next) => {
     user: req.user,
   });
 };
+
+module.exports.update_first_login = asyncHandler(async (req, res, next) => {
+  await prisma.user.update({
+    where: {
+      id: req.body.userId,
+    },
+    data: {
+      firstLogin: false,
+    },
+  });
+  res.status(200).json({ msg: "Updated!" });
+});
