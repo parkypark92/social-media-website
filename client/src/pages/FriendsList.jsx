092ff7b31facfd5533bf2b7b58c9d712914899b0
Andrew <115462877+parkypark92@users.noreply.github.com>
2025-12-03 16:59:24 +0000

    get rid of unnecessary semicolon

--- client/src/pages/FriendsList.jsx
+++ client/src/pages/FriendsList.jsx
@@ -4,7 +4,7 @@ import styles from "./FriendsList.module.css";
 export default function FriendsList() {
   return (
     <div className={styles.container}>
-      <FriendsListPreview></FriendsListPreview>;
+      <FriendsListPreview></FriendsListPreview>
     </div>
   );
 }
